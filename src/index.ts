0fccc450be0be1525b6985b0830923143580fdc5
Benjamin Otto <benjamin.otto@style-systems.de>
2017-09-25 17:35:58 +0200

    added missing import

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { Container } from './Container';
 export { inject } from './inject';
 export { Module } from './Module';
 export { Bind } from './Binder';
+export { createUncontrolledInject } from './createUncontrolledInject';
